2a35f2cb0926f506a4568b45d003f0ff90a57895
BilScott <billyboybean@gmail.com>
2025-04-20 18:20:33 +0100

    deploy rebuild

---
+++ build/static/js/952.9e9c4588.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkscottish_squash_masters=self.webpackChunkscottish_squash_masters||[]).push([[952],{952:s=>{s.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.9e9c4588.chunk.js.map
